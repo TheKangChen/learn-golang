44481fc11b5fc5eb4af0f3fe001e1ab6fde22474
TheKangChen <kangchen.work@gmail.com>
2025-10-30 23:26:51 -0400

    regexp

---
+++ 20_regexp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	searchIn := "John: 2578.34 William: 4567.23 Steve: 5632.18"
+	pat := "[0-9]+.[0-9]+"
+
+	// Match(): find match in byte string
+	ok, err := regexp.Match(pat, []byte(searchIn))
+	if err == nil {
+		fmt.Println(ok)
+	}
+
+	// Compile(): parses a regular expression and returns a regexp object if successful
+	// MustCompile(): panic if failed
+	re, err := regexp.Compile(pat)
+
+	// Find() & FindAll(): return matching slices of bytes
+	// FindString() & FindAllString(): strings
+	if err == nil {
+		matchedBytes := re.FindAll([]byte(searchIn), -1)
+		for _, b := range matchedBytes {
+			fmt.Printf("%s ", b)
+		}
+		fmt.Println()
+
+		matchedStrings := re.FindAllString(searchIn, -1)
+		fmt.Println(matchedStrings)
+	}
+
+	// If there are capture groups in the regex pattern, use FindSubmatch() to access them
+	// Index: [0] full match; [1:] groups
+	re = regexp.MustCompile(`a(x*)b(y|z)c`)
+	fmt.Printf("%q\n", re.FindStringSubmatch("-axxxbyc-"))
+	fmt.Printf("%q\n", re.FindStringSubmatch("-abzc-"))
+}
